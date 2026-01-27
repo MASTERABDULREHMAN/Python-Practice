bd2ab7d29b9caea45affd83f9958fe49ba868b92
MASTERABDULREHMAN <147626041+MASTERABDULREHMAN@users.noreply.github.com>
2026-01-27 09:23:17 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,54 @@
+# #function to count word frequency 
+# def word_fun():
+#     sentence=input("Enter any sentence")
+#     words=sentence.lower().split()
+
+#     word_count={}
+#     for word in words:
+#         if word in word_count:
+#             word_count[word]+=1
+#         else:
+#          word_count[word]=1
+#     print(word_count)
+
+
+# def word_frequency():
+#     senstence=input("enter any sentence")
+#     words=senstence.lower().split()
+
+#     word_count={}
+#     for word in words:
+#         if word in word_count:
+#             word_count[word] +=1
+#         else:
+#             word_count[word] =1
+#     print(word_count)
+# word_frequency()
+
+# def frequency_count():
+#     paragraph=input("enter any paragraph:..")
+#     sentence=paragraph.lower().split()
+#     word_count={}
+#     for word in sentence:
+#         if word in sentence:
+#             word_count[word]+=1
+#         else:
+#             word_count[word]=1
+#     print(word_count)
+# frequency_count()
+def frequency_count():
+    paragraph=input("enter any paragraph")
+    sentence=paragraph.lower().split()
+    print(sentence)
+frequency_count()
+
+
+
+
+
+
+
+
+
+
+
