0e602a7ea4df04539e3684caff63bcb0e7d4353d
MASTERABDULREHMAN <147626041+MASTERABDULREHMAN@users.noreply.github.com>
2026-01-28 11:36:31 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,9 @@
+# def multiplicationTable(N):
+#     #code here 
+#     number=input("enter any number...")
+#     for i in range(1,11):
+#       print(i*number)
+# multiplicationTable(5)
+N=int(input("N="))
+for i in range(5,51,5):
+ print(list(range((i))))
